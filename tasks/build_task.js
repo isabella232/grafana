ba5470de5515fe9458dd7ca816a36d2d300b9ec9
Chris Bentivenga <cbentivenga@lyft.com>
2017-02-02 17:20:34 -0800

    fix

--- tasks/build_task.js
+++ tasks/build_task.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
   "use strict";
+  grunt.loadNpmTasks('grunt-contrib-copy-force');
 
   // Concat and Minify the src directory into dist
   grunt.registerTask('build', [
